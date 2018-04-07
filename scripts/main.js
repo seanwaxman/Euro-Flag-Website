801a4beff17c3b6a16e8a5d7a73d8514cd3b07e7
Sean Waxman <32117761+seanwaxman@users.noreply.github.com>
2018-04-07 14:55:04 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,29 @@
 var myHeading = document.querySelector("h1");
 myHeading.textContent = "Flag Guesser Game";
 var myImage = document.querySelector("img");
+myImage = document.querySelector("img");
 myImage.onclick = function() {
 	var mySrc = myImage.getAttribute("src");
+	var pFlag = document.querySelector("pFlag")
 	if (mySrc === "images/bw.png")
 	{
+		myImage.setAttribute("src", "images/ca.png");
+		pFlag.textContent = "This is Canada's flag";
+	}
+	else if (mySrc === "images/ca.png")
+	{
+		myImage.setAttribute("src", "images/us.png");
+		pFlag.textContent = "This is the United States of America's flag"
+	}
+	else if (mySrc === "images/us.png")
+	{
 		myImage.setAttribute("src", "images/br.png");
+		pFlag.textContent = "This is Brazil's flag"
 	}
 	else
 	{
 		myImage.setAttribute("src", "images/bw.png");
+		pFlag.textContent = "This is Botswana's flag"
 	}
 }
 var myButton = document.querySelector("button");
